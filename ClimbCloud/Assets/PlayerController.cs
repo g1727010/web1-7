bba3c35db80b5c79b1b2f9f5d23a7c6e9b7d5698
Unknown <katsuma4869@icloud.com>
2018-06-11 17:44:10 +0900

    最初のコミット

---
+++ ClimbCloud/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigit2D;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        this.rigit2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigit2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        int key = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            key = 1;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            key = -1;
+
+        float speedx = Mathf.Abs(this.rigit2D.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigit2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2.0f;
+    }
+}
